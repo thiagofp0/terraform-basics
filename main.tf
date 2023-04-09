3fa966ed1c8ea421f1876ffcbb85a8c107cc2ce8
Thiago <thiagofp707@gmail.com>
2023-04-09 20:29:14 -0300

    Inicial

---
+++ main.tf
@@ -0,0 +1,28 @@
+
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "tfresources"
+  location = "westus2"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
